009838635b0467dc0abab4f47060d2dacc0b45da
lilikzone12 <121841940+lilikzone12@users.noreply.github.com>
2023-02-09 22:57:24 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "github.com/PrajjawalBanati/terraform-aws-s3-webapp"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
